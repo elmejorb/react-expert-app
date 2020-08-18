235932ecaa3d958b7d6f40e4c7a513b92806dad3
Luis Fernando Martinez <elmejorb@gmail.com>
2020-08-18 14:34:09 -0500

    doc agregado

---
+++ docs/precache-manifest.9bd0af9bd5e8dbd187996b424e9a0662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7a7d1d1cc5afbdeace102f308986f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8727086123b67758f57",
+    "url": "/static/css/main.c13cbfc4.chunk.css"
+  },
+  {
+    "revision": "1805387425b885f9d675",
+    "url": "/static/js/2.4b408a84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b408a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8727086123b67758f57",
+    "url": "/static/js/main.ff0af8aa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
